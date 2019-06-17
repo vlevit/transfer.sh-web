5d81ac563694bbc2e8b3bfb107685e2af6ea32f4
Vyacheslav Levit <dev@vlevit.org>
2019-06-17 21:55:09 +0300

    Rebrand to bucket.ftp.sh

--- src/scripts/mainbk.js
+++ src/scripts/mainbk.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     // Terminal typing animation
     $("#from-terminal p").typed({
-        strings: ["curl --upload-file ./hello.txt https://pirates.suka.se/hello.txt\nhttps://pirates.suka.se/66nb8/hello.txt \n "],
+        strings: ["curl --upload-file ./hello.txt https://bucket.ftp.sh/hello.txt\nhttps://bucket.ftp.sh/66nb8/hello.txt \n "],
         typeSpeed: 0,
         loop: true,
     });
